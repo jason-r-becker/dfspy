0ef7b9aca7004993567444a128b56aa33d032115
jason-r-becker <jasonrichardbecker@gmail.com>
2019-01-19 22:10:25 -0800

    added NFL.com and STATS

--- dfspy/scrape_data.py
+++ dfspy/scrape_data.py
@@ -1,8 +1,11 @@
 import argparse
+import json
 import os
 import sys
+from collections import defaultdict
 from datetime import datetime as dt
 from datetime import timedelta
+from itertools import chain
 from time import sleep
 
 import requests
@@ -15,10 +18,9 @@ from tqdm import tqdm
 
 def main():
     """
-    Scrape data for specified period. Data saved to
-    `../data/<year>/<week>/<source>.csv`.
+    Scrape data for specified period.
     
-    TODO: implement dataScraping class usage
+    Data saved to `../data/<year>/<week>/<position>/<source>.csv`.
     """
     # Disable warnings for unverified requests.
     requests.packages.urllib3.disable_warnings()
@@ -43,7 +45,8 @@ def parse_args():
     
     # Set default sources.
     # TODO: Update sources as their scrapers are written.
-    default_sources = ['CBS', 'ESPN', 'Yahoo']
+    default_sources = ['CBS', 'ESPN', 'FantasyPros', 'FFToday',
+                       'NFL', 'RTSports', 'STATS', 'Yahoo']
     
     # Get default season year and week from current time.
     today = dt.utcnow()
@@ -64,7 +67,7 @@ def parse_args():
         (today-pd.to_datetime(season_start)+timedelta(days=2)).total_seconds()
         / (3600 * 24 * 7)
         ))
-    default_week = max(1, min(16, default_week))
+    default_week = max(1, min(17, default_week))
     
     # Set default arguments.
     parser.set_defaults(
@@ -94,13 +97,38 @@ def mkdir(directory):
     except OSError:
         pass
         
+def scrape_NFL_json_col_names():
+    """Get column names for NFL.com json files."""
+    url = 'https://api.fantasy.nfl.com/v1/game/stats?format=json'
+    # Scrape data, attempt 5 times before passing.
+    attempts = 5
+    for attempt in range(attempts):
+        try:
+            r = requests.get(url, verify=False, timeout=5)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except Exception as e:
+            sleep(1)
+            r = None
+            continue
+        else:
+            break
+        
+    if (r is not None and
+        r.status_code == 200 and
+        'stats' in r.json()):
+        # Succesfull scrape; get column names.
+        col_names = {sub_dict['id']: sub_dict['shortName'] \
+                     for sub_dict in r.json()['stats']}
+        
+        # Output to json file.
+        with open('../data/.NFL_json_columns.json', 'w') as fid:
+            json.dump(col_names, fid, indent=4, sort_keys=True)
         
 class dataScraper:
     """
     Class for scraping weekly or season projections from various
     web sources and saving to the `../data` directory.
-    
-    TODO: call singleScrape and build data directory for given inputs.
     
     Parameters
     ----------
@@ -130,14 +158,60 @@ class dataScraper:
         self.years = years if type(years) in [list, tuple] else [years]
         self.positions = 'QB RB WR TE DST K'.split()
         if weeks == 'all':
-            self.weeks = np.arange(17)
+            self.weeks = np.arange(18)
         elif weeks == 'season':
             self.weeks = [0]
         elif type(weeks) in [list, tuple]:
             self.weeks = weeks
         else:
             self.weeks = [weeks]
+        
+        self._headers = {}
+        with open('../data/.NFL_json_columns.json', 'r') as fid:
+            self._headers['NFL'] = json.load(fid)
+            self._headers['STATS'] = self._headers['NFL']
             
+    def _validate_current_period(self, source, week, year, pos):
+        """Return True if data can be scraped else False."""
+        week, year = int(week), int(year)
+        if year < 2016:
+            return False
+        
+        # No weekly histoirical Data.
+        no_weekly_data = {
+            'ESPN': [2016, 2017],
+            'FantasyPros': [2016, 2017],
+            'NFL': [2016, 2017],
+            'RTSports': [2016, 2017, 2018],
+            'Yahoo': [2016, 2017],
+            }
+        try:
+            if week > 0 and year in no_weekly_data[source]:
+                return False
+        except KeyError:
+            pass
+        
+
+        # No full season data.
+        no_full_season_data = {
+            'ESPN': [2016],
+            'FantasyPros': [2016, 2017],
+            'NFL': [2016, 2017],
+            'RTSports': [2016, 2017],
+            'Yahoo': [2016, 2017],
+            }
+        try:
+            if week == 0 and year in no_full_season_data[source]:
+                return False
+        except KeyError:
+            pass
+        
+        # No weekly DST for FFToday.
+        if source == 'FFToday' and week > 0 and pos == 'DST':
+            return False
+        
+        return True
+        
     def save(self):
         """Scrape and save specified data."""
         iters = len(self.years) * len(self.weeks) * len(self.positions) \
@@ -147,14 +221,33 @@ class dataScraper:
                 for week in self.weeks:
                     for pos in self.positions:
                         for source in self.sources:
-                            scraper = singleScrape(source, week, year, pos)
+                            try:
+                                header = self._headers[source]
+                            except KeyError:
+                                header = None
+                            if not self._validate_current_period(
+                                source, week, year, pos):
+                                # Data does not exist.
+                                pbar.update(1)
+                                continue
+                                
+                            # Data exists; scrape and save.
+                            scraper = singleScrape(
+                                source, week, year, pos, header)
                             df = scraper.scrape()
-                            if df is not None:
+                            if df is None:
+                                msg = (
+                                    f'Warning: Scrape failed for {pos} from '
+                                    f'{source} Week {week} - {year}.'
+                                    )
+                                print(msg)
+                            else:
                                 path = f'../data/{year}/{week}/{pos}'
                                 mkdir(path)
                                 df.to_csv(f'{path}/{source}.csv', index=False)
                             pbar.update(1)
 
+# %%
 
 class singleScrape:
     """
@@ -162,9 +255,7 @@ class singleScrape:
     
     TODO: add scraping for:
         - FantasyData
-        - Fantasypros
         - FantasySharks
-        - FFToday
         - FleaFlicker
         - NumberFire
         - FantasyFootballNerd
@@ -182,18 +273,20 @@ class singleScrape:
         Year of the season to scrape
     pos: str
         Position to scrape.
-    
+    header: dict, default=None
+        Dictionary conversion of headers for given source.
     Methods
     -------
     scrape(): Returns scraped data as DataFrame.
     """
     
-    def __init__(self, source, week, year, pos):
+    def __init__(self, source, week, year, pos, header=None):
         self.source = source
         self.week = week
         self.year = year
         self.pos = pos
-    
+        self.header = header
+        
     def _get_url(self):
         """Returns url for scraping."""
         return eval(f'self._url_for_{self.source}()')
@@ -229,17 +322,26 @@ class singleScrape:
         page = f'&startIndex={int(self._page * 40)}'
         url = (
             f'http://games.espn.com/ffl/tools/projections?projections?='
-            f'slot=CategoryId=0{cat_id}&{period}&seasonId={self.year}{page}'
+            f'slot=CategoryId=0{cat_id}{page}&{period}&seasonId={self.year}'
             )
         return url
         
     def _clean_ESPN_df(self, df):
-        header_ix = list(df.iloc[:, 0]).index('PLAYER, TEAM POS')
+        try:
+            header_ix = list(df.iloc[:, 0]).index('PLAYER, TEAM POS')
+        except ValueError:
+            header_ix = list(df.iloc[:, 1]).index('PLAYER, TEAM POS')
+
         cols = [col for col in df.iloc[header_ix, :] if isinstance(col, str)]
         clean_df = df.iloc[header_ix+1:, :len(cols)].copy()
-        cols[0] = 'Player'
-        clean_df.columns = cols
-        
+        if 'RNK' in cols:
+            cols[1] = 'Player'
+            clean_df.columns = cols
+            clean_df.drop('RNK', inplace=True, axis=1)
+        else:
+            cols[0] = 'Player'
+            clean_df.columns = cols
+    
         clean_df.index = list(range(len(clean_df)))
         clean_df['POS'] = self.pos
         return clean_df
@@ -266,6 +368,133 @@ class singleScrape:
         clean_df['POS'] = self.pos
         return clean_df
         
+    def _url_for_FantasyPros(self):
+        pos = self.pos.lower()
+        period = 'draft' if self.week == 0 else self.week
+        url = (
+            f'https://www.fantasypros.com/nfl/projections/{pos}'
+            f'.php?week={period}'
+            )
+        return url
+        
+    def _clean_FantasyPros_df(self, df):
+        # Drop first multi-index row if it there are multiple levels.
+        try:
+            levels = len(df.columns.levels)
+        except AttributeError:
+            pass
+        else:
+            df.columns = df.columns.droplevel(0)
+        df['POS'] = self.pos
+        return df
+        
+    def _url_for_FFToday(self):
+        pos = {'QB': 10, 'RB': 20, 'WR': 30, 'TE': 40, 'DST': 99, 'K': 80}
+        period = '' if self.week == 0 else f'&GameWeek={self.week}&'
+        wk = '' if self.week == 0 else ''
+        url = (
+            f'http://www.fftoday.com/rankings/player{wk}proj.php?'
+            f'Season={self.year}{period}&PosID={pos[self.pos]}&LeagueID=1'
+            f'&order_by=FFPts&sort_order=DESC&cur_page={self._page}'
+        )
+        return url
+        
+    def _clean_FFToday_df(self, df):
+        try:
+            header_ix = list(df.iloc[:, 1]).index('Player Sort First: Last:')
+        except ValueError:
+            header_ix = list(df.iloc[:, 1]).index('Team')
+        cols = [col for col in df.iloc[header_ix, :] if isinstance(col, str)]
+
+        clean_df = df.iloc[header_ix+1:, :len(cols)].copy()
+        cols[1] = 'Player'
+        clean_df.columns = cols
+        clean_df.drop('Chg', inplace=True, axis=1)
+        return clean_df
+    
+    def _url_for_RTSports(self):
+        pos = {'QB': 0, 'RB': 1, 'WR': 2, 'TE': 3, 'DST': 5, 'K': 4}[self.pos]
+        url = (
+            f'https://www.freedraftguide.com/football/draft-guide-rankings'
+            f'-provider.php?POS={pos}'
+            )
+        return url
+
+    def _get_RTSports_df(self, raw_dict):
+        """Get RTSports from json web source and convert to DataFrame."""
+        # Get json keys for top and 'stats' levels of json.
+        bad_keys = ['name', 'year', 'team', 'games_played']
+        level_1_keys = ['name', 'nfl_team', 'college', 'experience',
+                         'draft_year', 'draft_round', 'draft_pick']
+        level_2_keys = [key for key in raw_dict['1']['stats'].keys() \
+                         if key not in bad_keys]
+        
+        # Build dictionary of specified stats.
+        clean_dict = defaultdict(list)
+        for key in raw_dict.keys():
+            for sub_key in level_1_keys:
+                clean_dict[sub_key].append(raw_dict[key][sub_key])
+            for sub_key in level_2_keys:
+                clean_dict[sub_key].append(raw_dict[key]['stats'][sub_key])
+
+        # Convert to DataFrame and clean.
+        df = pd.DataFrame(clean_dict)
+        df.rename(columns={'name': 'Player'}, inplace=True)
+        df['POS'] = self.pos
+        return df
+        
+    def _url_for_NFL(self):
+        pos = 'DEF' if self.pos == 'DST' else self.pos
+        period = f'seasonProjectedStats&season={self.year}' if self.week == 0 \
+                 else f'weekProjectedStats&season={self.year}&week={self.week}'
+        url = (
+            f'http://api.fantasy.nfl.com/v1/players/stats?statType={period}'
+            f'&position={pos}&format=json'
+            )
+        return url
+
+    def _url_for_STATS(self):
+        pos = 'DEF' if self.pos == 'DST' else self.pos
+        period = f'seasonStats&season={self.year}' if self.week == 0 \
+                 else f'weekStats&season={self.year}&week={self.week}'
+        url = (
+            f'http://api.fantasy.nfl.com/v1/players/stats?statType={period}'
+            f'&position={pos}&format=json'
+            )
+        return url
+        
+    def _get_NFL_df(self, raw_dict):
+        """Scrape NFL.com projections from json and convert to DataFrame."""
+        # Find all unique stats which occur in given json.
+        unique_cols_ix = list(set(chain(
+            *[list(sub_dict['stats'].keys()) for sub_dict in raw_dict])))
+        cols_ix = list(map(str, sorted(map(int, unique_cols_ix))))
+        cols = [self.header[ix] for ix in cols_ix]
+        
+        # Find duplicate columns and remove them from list of stored columns.
+        duplicate_cols = list(set([col for col in cols if cols.count(col) > 1]))
+        for dup_col in duplicate_cols:
+            ix = cols.index(dup_col)
+            cols.pop(ix)
+            cols_ix.pop(ix)
+        
+        # Build dictionary of specified stats.
+        clean_dict = defaultdict(list)
+        for sub_dict in raw_dict:
+            for sub_key in ['name', 'teamAbbr']:
+                clean_dict[sub_key].append(sub_dict[sub_key])
+            for col, ix in zip(cols, cols_ix):
+                try:
+                    clean_dict[col].append(float(sub_dict['stats'][ix]))
+                except KeyError:
+                    clean_dict[col].append(0)
+
+        # Convert to DataFrame and clean.
+        df = pd.DataFrame(clean_dict)
+        df.rename(columns={'name': 'Player', 'teamAbbr': 'Team'}, inplace=True)
+        df['POS'] = self.pos
+        return df
+        
     def _scrape_single_page(self):
         """
         Scrape DataFrame for specified inits.
@@ -289,18 +518,36 @@ class singleScrape:
             else:
                 break
         
-        # Print Warning if scrape failed all attempts.
         if r is None:
-            msg = (
-                f'Warning: Scrape failed for {self.pos} from {self.source} '
-                f'Week {self.week} - {self.year}.'
-                )
-            print(msg)
             return None
+        
+        if self.source == 'RTSports':
+            try:
+                df = self._get_RTSports_df(r.json()['player_list'])
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except Exception as e:
+                df = None
+            return df
+        elif self.source in ['NFL', 'STATS']:
+            try:
+                df = self._get_NFL_df(r.json()['players'])
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except Exception as e:
+                df = None
+                print(sys.exc_info()[0])
+                print(e)
+            return df
         
         # Convert scrape into DataFrame and clean.
         soup = BeautifulSoup(r.text, features='lxml')
-        table_ix = 1 if self.source in ['Yahoo'] else 0
+        if self.source in ['Yahoo']:
+            table_ix = 1
+        elif self.source in ['FFToday']:
+            table_ix = 3
+        else:
+            table_ix = 0
         table = soup.find_all('table')[table_ix]
         df = pd.read_html(str(table))[0]
         return self._clean_scraped_df(df)
@@ -316,7 +563,7 @@ class singleScrape:
         df: pd.DataFrame
             DataFrame of full resulting scrape, None if scrape fails.
         """
-        multi_scrape_sources = ['ESPN', 'Yahoo']
+        multi_scrape_sources = ['ESPN', 'Yahoo', 'FFToday']
         if self.source in multi_scrape_sources:
             max_pages = 10
             df_pages = []
@@ -328,6 +575,7 @@ class singleScrape:
                 except (KeyboardInterrupt, SystemExit):
                     raise
                 except Exception as e:
+                    # print(e)
                     break
                 
                 if not isinstance(df_i, pd.DataFrame):
@@ -356,11 +604,18 @@ class singleScrape:
             df = self._scrape_single_page()
         
         return df
-        
-        
 
 
 # %%
 
+
+
 if __name__ == '__main__':
     main()
+
+
+
+
+        
+
+    
