863fd0b12eddfdca583bc4b175501c36bba418a6
jason-r-becker <jasonrichardbecker@gmail.com>
2019-03-17 18:09:35 -0700

    Added machine learning algorithms

--- dfspy/train_projections.py
+++ dfspy/train_projections.py
@@ -1,15 +1,82 @@
+import argparse
+import os
+import warnings
 from collections import defaultdict
+from glob import glob
 from itertools import chain
+from time import perf_counter
 
 import empiricalutilities as eu
+import joblib
 import numpy as np
 import pandas as pd
+import pickle
 import statsmodels.api as sms
+from operator import itemgetter
+from scipy.stats import randint, uniform, expon
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import ElasticNet
+from sklearn.model_selection import StratifiedKFold, RandomizedSearchCV
+from sklearn.metrics import classification_report
+from sklearn.svm import SVR
+from sklearn.preprocessing import StandardScaler
+from tqdm import tqdm
 
 from project_stats import StatProjection
 
 # %%
 
+def main():
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    warnings.simplefilter(action='ignore', category=DeprecationWarning)
+    make_model_dirs()
+
+    # Read pickled data if it exists, otherwise generate imputed data.
+    positions = 'QB RB WR TE DST'.split()
+    try:
+        with open('../data/.imputed_data', 'rb') as fid:
+            data = pickle.load(fid)
+    except FileNotFoundError:
+        trainer_dict = {pos: TrainProjections(pos) for pos in positions}
+        data = {pos: trainer_dict[pos]._stat_dfs for pos in positions}
+        # Store imputed data.
+        with open('../data/.imputed_data', 'wb') as fid:
+            pickle.dump(data, fid)
+    trainer_dict = {
+        pos: TrainProjections(pos, data=data[pos]) for pos in positions}
+    
+    # Train model on essential stats.
+    args = parse_args()
+    if args.model == 'WEIGHTED':
+        train_simple_linear_regression(
+            trainers=trainer_dict,
+            period=args.period,
+            save=args.save,
+            verbose=args.verbose,
+            )
+    else:
+        train_ml_models(
+            model=args.model,
+            trainers=trainer_dict,
+            n_iters=args.n_iters,
+            period=args.period,
+            save=args.save,
+            verbose=args.verbose,
+            )
+
+
+def parse_args():
+    """Collect settings from command line and set defaults."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model', help='Model Name')
+    parser.add_argument('-p', '--period', help='weekly/season')
+    parser.add_argument(
+        '-n', '--n_iters', type=int, help='Number of training iterations')
+    parser.add_argument('-s', '--save', action='store_true', help='Save Models')
+    parser.add_argument(
+        '-v', '--verbose', action='store_true', help='Verbosity')
+    parser.set_defaults(model='LR', period='weekly', n_iters=500)
+    return parser.parse_args()
 
 class TrainProjections:
     """
@@ -27,6 +94,12 @@ class TrainProjections:
     data: dict[stat: pd.DataFrame], default=None
         Dictionary of all stats source projections and true realized stats
         for each projected stat.
+    
+    Methods
+    -------
+    train_simple_linear_regression(): train WEIGHTED simple linear
+        regression model.
+    
     """
     
     def __init__(self, pos, year=2018, season=False, data=None):
@@ -35,9 +108,11 @@ class TrainProjections:
         self._train_pct = 0.8
         
         if self.season_flag:
+            self.period = 'season'
             # TODO
             pass
         else:
+            self.period = 'weekly'
             projector = StatProjection(pos, year, season)
             self.essential_stats = projector.essential_stats
             self.nonessential_stats = projector.nonessential_stats
@@ -49,11 +124,11 @@ class TrainProjections:
             else:
                 self._stat_dfs = data.copy()
         
-        self._train_dfs, self._test_dfs = self._train_test_split()
+        self._train_dfs, self._cv_dfs, self._test_dfs = self._train_test_split()
         
     def _train_test_split(self):
-        """Split stat DataFrames into train/test dicts above threshold."""
-        train_dfs, test_dfs = {}, {}
+        """Split stat DataFrames into train/cv/test dicts above threshold."""
+        train_dfs, cv_dfs, test_dfs = {}, {}, {}
         for stat in self.essential_stats:
             df = self._stat_dfs[stat]
             df_no_weeks = df.drop('Week', axis=1)
@@ -64,12 +139,13 @@ class TrainProjections:
                 # TODO
                 pass
             else:
-                train_dfs[stat] = df[df['Week'] < 15].copy()
+                train_dfs[stat] = df[df['Week'] < 12].copy()
+                cv_dfs[stat] = df[(df['Week'] >= 12) & (df['Week'] < 15)].copy()
                 test_dfs[stat] = df[df['Week'] >= 15].copy()
 
-        return train_dfs, test_dfs
+        return train_dfs, cv_dfs, test_dfs
 
-    def _train_simple_linear_regression(self, stat):
+    def train_simple_linear_regression(self, stat):
         """
         Train simple linear regression model of specified stat.
         
@@ -86,36 +162,149 @@ class TrainProjections:
         """
         
         train_df = self._train_dfs[stat]
-        # Split training portion for simple regression.
-        train_df = train_df.iloc[:int(self._train_pct*len(train_df)), :]
         ignored_cols = ['Player', 'Team', 'Pos', 'POS', 'Week', 'STATS']
-        cols = [col for col in list(train_df) if col not in ignored_cols]
+        cols = [col for col in sorted(train_df) if col not in ignored_cols]
         
         X = train_df[cols].copy()
         y = train_df['STATS'].values
         
         return sms.OLS(y, sms.add_constant(X)).fit()
+
+    def train_machine_learning_algorithm(self, model, stat, n_iters, kfolds=5):
+        """
+        Train simple linear regression model of specified stat.
         
-#
-# self = TrainProjections('QB')
-# data = self._stat_dfs
+        Parameters
+        ----------
+        stat: str
+            Stat to train model for.
+        model: {'LR', 'SVR', 'RF'}
+            Machine learning algorithm to train.
+                - LR: Projection stats are computed using advanced linear
+                      regression with regularization.
+                - SVR: Projection stats are computed with
+                       support vector machines.
+                - RF: Projection stats are computed using a random forest.
+        n_iters: int
+            Number of training iterations.
+        kfolds: int, default=5
+            Number of kfolds to use for cross-validation.
+            
+        Returns
+        -------
+        final_model: sklearn model class
+            Best model from cross-validation training.
+        cv_res: dict
+            Cross-validation results dict.
+        """
+        # Get X and y data for machine learning models.
+        train_df = self._train_dfs[stat]
+        ignored_cols = ['Player', 'Team', 'Pos', 'POS', 'Week', 'STATS']
+        cols = [col for col in sorted(train_df) if col not in ignored_cols]
+        
+        X = train_df[cols].values
+        y = train_df['STATS'].values
+        
+        # Standardize data.
+        # Load scaler if it has been saved, else scale and save.
+        fid = f"../data/.models/{self.period}/{self.pos}/" \
+            f"{stat.replace(' ', '_')}/scaler.sav"
+        try:
+            scaler = joblib.load(fid)
+        except FileNotFoundError:
+            scaler = StandardScaler().fit(X)
+            joblib.dump(scaler, fid)
+        
+        X_scaled = scaler.transform(X)
+        
+        # Make stratified kfolds from deciles of stat values.
+        y_bins = pd.qcut(y, 10, labels=False, duplicates='drop')
+        y_bins = np.array(
+            pd.qcut(pd.Series(y).rank(method='first'), 10, labels=False))
+        skfolds = StratifiedKFold(
+            n_splits=kfolds, shuffle=True, random_state=8675309)
+        
+        # Set up parameters for CV search.
+        params = {
+            'LR': {'alpha': expon(), 'l1_ratio': uniform()},
+            'SVR': {'kernel': ['rbf'], 'C': expon(), 'gamma': expon()},
+            'RF': {
+                'n_estimators': randint(low=1, high=100),
+                'max_depth': randint(low=2, high=6),
+                }
+            }[model]
+        mod = {
+            'LR': ElasticNet,
+            'SVR': SVR,
+            'RF': RandomForestRegressor,
+            }[model]
+        
+        # Train model with cross-validation.
+        rnd_search = RandomizedSearchCV(
+            mod(),
+            param_distributions=params,
+            n_iter=n_iters,
+            cv=skfolds.split(X_scaled, y_bins),
+            scoring='neg_mean_absolute_error',
+            )
+        
+        rnd_search.fit(X_scaled, y)
+        final_model = rnd_search.best_estimator_
+        cv_res = rnd_search.cv_results_
 
-self = TrainProjections('QB', data=data)
-
+        return final_model, cv_res
+        
+        
+# pos = 'QB'
+# stat = 'Pass Yds'
+# self = TrainProjections(pos, data=data[pos])
+# model = 'RF'
+# model = 'SVR'
 # %%
 
+def mkdir(directory):
+    """Make directory if it does not already exist."""
+    try:
+        os.makedirs(directory)
+    except OSError:
+        pass
 
-pos = 'QB'
-stat = 'Pass Yds'
 
-
-
-def make_simple_linear_regression_results_table():
-    """Make linear regression results table for appendix."""
+def make_model_dirs():
     positions = 'QB RB WR TE DST'.split()
-    trainers = {pos: TrainProjections(pos) for pos in positions}
+    for pos in positions:
+        projector = StatProjection(pos)
+        stats = projector.essential_stats + projector.nonessential_stats
+        for stat in stats:
+            mkdir(f"../data/.models/weekly/{pos}/{stat.replace(' ', '_')}")
+            mkdir(f"../data/.models/season/{pos}/{stat.replace(' ', '_')}")
+        
+        
+def train_simple_linear_regression(
+    trainers, period, n_iters, save, verbose):
+    """
+    Train linear regression weighted model.
     
-    def pval_significance(pvals):
+    Parameters
+    ----------
+    trainers: dict
+        Dict with postions as keys and TrainProjections class as values.
+    period: {'weekly', 'season'}
+        Period to train models over.
+    n_iters: int
+        Number of iterations to train models.
+    save: bool
+        If True, save results into data/.models dir.
+    verbose: bool
+        If True, print progress bar to screen.
+    """
+    
+    if verbose:
+        print('\n\nSimple linear regresssion WEIGHTED model:')
+        t0 = perf_counter()
+    positions = trainers.keys()
+    
+    def fmt_pval(pvals):
         """Return *-style significance list for pvalues."""
         sig = []
         for pval in pvals:
@@ -131,17 +320,28 @@ def make_simple_linear_regression_results_table():
     
     # Get OLS results for each essential stat.
     df = pd.DataFrame()
-    for pos in positions:
-        for stat in trainers[pos].essential_stats:
-            ols = trainers[pos]._train_simple_linear_regression(stat)
-            # Find signifcance and rounded values.
-            vals = [pos, stat] + [f'{val}{sig}' for val, sig in \
-                    zip(ols.params.round(2), pval_significance(ols.pvalues))]
-            cols = ['Position', 'Stat'] + list(ols.params.index)
-            temp_df = pd.DataFrame([vals], columns=cols)
-            df = df.append(temp_df, sort=False)
-            
-    
+    dis = not verbose
+    n = sum(len(trainer.essential_stats) for trainer in trainers.values())
+    with tqdm(total=n, disable=dis) as pbar:
+        for pos in positions:
+            for stat in trainers[pos].essential_stats:
+                ols = trainers[pos].train_simple_linear_regression(stat)
+                # Find signifcance and rounded values.
+                vals = [pos, stat] + [f'{val}{sig}' for val, sig in \
+                        zip(ols.params.round(2), fmt_pval(ols.pvalues))]
+                cols = ['Position', 'Stat'] + list(ols.params.index)
+                temp_df = pd.DataFrame([vals], columns=cols)
+                df = df.append(temp_df, sort=False)
+                
+                if save:
+                    fid = f'../data/.models/{period}/{pos}/' \
+                        f'{stat.replace(" ", "_")}/WEIGHTED.csv'
+                        
+                    save_df = pd.DataFrame(ols.params, columns=['vals'])
+                    save_df.to_csv(fid)
+                    
+                pbar.update(1)
+                
     # Change positon column to only indicate position once.
     n_pos = {pos: len(trainers[pos].essential_stats)-1 for pos in positions}
     pos_list = list(chain(*[[pos] + n_pos[pos]*[''] for pos in positions]))
@@ -153,6 +353,61 @@ def make_simple_linear_regression_results_table():
     cap += 'source. Asterisks denote statistical significance of regression '
     cap += 'coefficients, (*) denoting a p-value less than 0.5, (**) for less '
     cap += 'than 0.01, and (***) for less than 0.001.'
-    eu.latex_print(df, hide_index=True, adjust=True, col_fmt=col_fmt,
-                   caption=cap)
+    if verbose:
+        print(f'Time to train: {(perf_counter() - t0) / 60:.1f} min.')
+        eu.latex_print(
+            df, hide_index=True, adjust=True, col_fmt=col_fmt, caption=cap)
     
+        
+def train_ml_models(
+    trainers, model, n_iters, period, save, verbose):
+    """
+    Train linear regression weighted model.
+    
+    Parameters
+    ----------
+    trainers: dict
+        Dict with postions as keys and TrainProjections class as values.
+    model: {'LR', 'SVR', 'RF'}
+        Machine learning algorithm to train.
+            - LR: Projection stats are computed using advanced linear
+                  regression with regularization.
+            - SVR: Projection stats are computed with
+                   support vector machines.
+            - RF: Projection stats are computed using a random forest.
+    period: {'weekly', 'season'}
+        Period to train models over.
+    save: bool
+        If True, save results into data/.models dir.
+    verbose: bool
+        If True, print progress bar to screen.
+    """
+
+    positions = trainers.keys()
+    n = sum(len(trainer.essential_stats) for trainer in trainers.values())
+    with tqdm(total=n, disable=(not verbose)) as pbar:
+        for pos in positions:
+            for stat in trainers[pos].essential_stats:
+                if verbose:
+                    tqdm.write(f'')
+                    t0 = perf_counter()
+                    
+                mod, res = trainers[pos].train_machine_learning_algorithm(
+                    model, stat, n_iters)
+                
+                if save:
+                    fid = f'../data/.models/{period}/{pos}/' \
+                        f'{stat.replace(" ", "_")}/{model}.sav'
+                    joblib.dump(mod, fid)
+                    
+                if verbose:
+                    msg = f'{model} for {pos}-{stat}: ' \
+                        f'\tTime: {perf_counter() - t0:.1f} sec' \
+                        f"\tMAE: {-np.max(res['mean_test_score']):0.2f}"
+                    tqdm.write(msg)
+                pbar.update(1)
+
+
+if __name__ == '__main__':
+    main()
+# %%
